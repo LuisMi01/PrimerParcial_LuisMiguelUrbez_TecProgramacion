faf1466ab9a1f1241102a9063f5b15f375e2efb3
LuisMi01 <lmurbezv@gmail.com>
2023-02-23 19:25:34 +0100

    Terminando practica 1

---
+++ src/main/java/Practica1/Mates/Matematicas.java
@@ -0,0 +1,25 @@
+package Practica1.Mates;
+
+public class Matematicas {
+    //public static double generarNumeroPi(long pasos){ return 0;
+
+    public static double generarNumeroPi(long pasos){
+        int puntosTotales = 0;
+        int puntosCirculo = 0;
+        double x;
+        double y;
+        double pi;
+        for (int i = 0; i < pasos; i++){
+            x = Math.random();
+            y = Math.random();
+            if (Math.pow(x, 2) + Math.pow(y, 2) <= 1){
+                puntosCirculo++;
+            }
+            puntosTotales++;
+        }
+        pi = (double) 4 * puntosCirculo / puntosTotales;
+        return pi;
+    }
+
+
+}
